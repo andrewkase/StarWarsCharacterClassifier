399b455df553f5bcc75c62ce68c2517ee5a4dbff
andrewkase <158593896+andrewkase@users.noreply.github.com>
2025-04-04 10:14:46 -0400

    Update Create_CSV.py

--- Create_CSV.py
+++ Create_CSV.py
@@ -4,24 +4,39 @@ import csv
 # Define input and output directories
 image_dir = "Train_Images"
 
-file_path = 'Labels.csv'
+train_path = 'TrainingLabels.csv'
+validation_path = 'ValidLabels.csv'
 
 data = []
+data2 = []
 
 # Loop through each name folder inside Train_Images
 for character in os.listdir(image_dir):
     folder_path = os.path.join(image_dir, character)
 
+    i=0
     # Process each image in the character name folder
     for img_name in os.listdir(folder_path):
-        data.append([img_name, character])
+        if i < 2:
+            data2.append([img_name, character])
+        else:
+            data.append([img_name, character])
+        i+=1
         
 
-with open(file_path, 'w', newline='') as file:
+with open(train_path, 'w', newline='') as file:
     writer = csv.writer(file)
 
     writer.writerow(['ID', 'Character'])
 
     writer.writerows(data)
 
-print(f"CSV file '{file_path}' created successfully.")
+with open(validation_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    writer.writerow(['ID', 'Character'])
+
+    writer.writerows(data2)
+
+print(f"CSV file '{train_path}' created successfully.")
+print(f"CSV file '{validation_path}' created successfully.")
